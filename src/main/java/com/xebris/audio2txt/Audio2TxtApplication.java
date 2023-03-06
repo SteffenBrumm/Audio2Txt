4810cf483060613fabc233906f850648451e1c99
steffenbrumm <steffen.brumm@xebris.com>
2023-03-06 11:09:15 +0100

    Initialimport

---
+++ src/main/java/com/xebris/audio2txt/Audio2TxtApplication.java
@@ -0,0 +1,13 @@
+package com.xebris.audio2txt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Audio2TxtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Audio2TxtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xebris/audio2txt/Audio2TxtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xebris.audio2txt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Audio2TxtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
